38678cfb109409d77d4e5fb792a276e957cadd52
Winnie <wimacoj@gmail.com>
2023-10-03 15:38:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const leastFavoriteCustomer = "Peter";
+
+function test(){
+    return customerName;
+}
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+
+function setBestCustomer(value = "not bob"){
+     window.bestCustomer = value;
+}
+
+
+
+function overwriteBestCustomer(value = "maybe bob"){
+    window.bestCustomer = value;
+    //return bestCustomer;
+}
+
+
+function changeLeastFavoriteCustomer(value="Paul"){
+      leastFavoriteCustomer = value;
+     //return leastFavoriteCustomer;
+}
+
+
